dc44122a9a1681f15d2318422592c03f31745130
SK <sachin.f.kumar@capgemini.com>
2022-07-27 13:21:49 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import {useEffect, useState} from "react";
 import './App.css';
+import MovieCard from "./MovieCard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import SearchIcon from './search.svg';
+
+//const category = 'business';
+
+const apiURL = "http://www.omdbapi.com/?apikey=ccd6f6a3";//"https://newsapi.org/v2/everything?apiKey=92e5f629708d454f9c3ccea4328e94d6";
+
+const newsData1 = {
+    "author": "Constantin Gajura",
+    "source": {
+        "Id": null,
+        "Name": "Entrepreneur"
+    },
+    "publishedAt": "2022-06-29T19:30:00Z",
+    "url": "https://www.entrepreneur.com/article/428615"
 }
 
+const App = ()=>{
+
+    const [newsData, setNewsData]= useState([]);
+    const [searchTerm, setSearchTerm] =  useState("");
+
+    const searchNews = async (keyword)=>{
+        const response = await fetch(`${apiURL}&s=${keyword}`);
+        const data =  await response.json();
+        setNewsData(data.Search);
+        console.log(data.Search);
+    }
+
+    useEffect(()=>{
+        searchNews('Ironman');
+    },[]);
+
+
+    return (
+        <div className="app">
+             <h1>MOVIES WORLD</h1>
+            <div className="search">
+                <input 
+                placeholder="Search for Movies"
+                value={searchTerm}
+                onChange={(e)=>setSearchTerm(e.target.value)}
+                />
+                <img 
+                src={SearchIcon}
+                alt = "search news"
+                onClick={()=>searchNews(searchTerm)}
+                />
+            </div>
+            {
+                newsData?.length>0
+                ?(<div className="container">
+                    {
+                        newsData.map((newsData)=>(
+                            <MovieCard newsData= {newsData} />
+                        ))
+                    }
+              
+                 </div>)
+                 :(
+                    <div className="empty"> 
+                     <h2>No Movies found</h2>
+                    </div>
+                 )
+
+            }
+
+           
+        </div>
+       
+    );
+}
 export default App;
